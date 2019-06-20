7e3c617f88a7f0b2ff1e926ba36e8bc3d51efb00
Meggan <meggtriplett@gmail.com>
2019-06-20 00:58:33 -0700

    Adjusted abstration on classes and created two interfaces

---
+++ Zoo/Zoo/Interfaces/IEggsLayable.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace zoo.Interfaces
+{
+    interface IEggsLayable
+    {
+        string WeeksUntilHatch();
+
+        int NumberOfEggs();
+    }
+}
+
